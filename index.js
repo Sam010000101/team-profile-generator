8a8beaa77f02d12e8cf71f0a673bf6555d4e1abe
Sam B <reposme@outlook.com>
2023-01-28 12:58:53 +0000

    Added conditional questions for when Engineer is chosen, just working out how to return to add-or-buildto choose what to do next

--- index.js
+++ index.js
@@ -12,27 +12,28 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+console.log("Please build your team");
 
-const questionsTeamManager = [
+const questions = [
     {
         type: 'input',
-        name: 'team_manager_name',
-        message: "What's your name?",
+        name: 'team_managers_name',
+        message: "What is your team manager's name?",
     },
     {
         type: 'input',
         name: 'team_manager_id',
-        message: "What's your employee ID?",
+        message: "What is your team manager's id?",
     },
     {
         type: 'input',
         name: 'team_manager_email_address',
-        message: "What's your email address?",
+        message: "What is your team manager's email address?",
     },
     {
         type: 'input',
         name: 'team_manager_phone',
-        message: "What's your office phone number?",
+        message: "What is your team manager's office phone number?",
         validate(value) {
             const pass = value.match(
                 /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
@@ -54,11 +55,41 @@ const questionsTeamManager = [
             "Finish building the team?"
         ]
 
-    }
+    },
 
+    {
+        type: 'input',
+        name: 'engineers_name',
+        message: "What is your engineer's name?",
+        when: (answers => answers['add_or_build'] === "Add an engineer?")
+    },
+
+    {
+        type: 'input',
+        name: 'engineers_id',
+        message: "What is your engineer's id?",
+        when: (answers => answers['add_or_build'] === "Add an engineer?")
+    },
+
+    {
+        type: 'input',
+        name: 'engineers_email',
+        message: "What is your engineer's email address?",
+        when: (answers => answers['add_or_build'] === "Add an engineer?")
+    },
+
+    {
+        type: 'input',
+        name: 'engineers_github',
+        message: "What is your engineer's GitHub username?",
+        when: (answers => answers['add_or_build'] === "Add an engineer?")
+    },
+
+
+    
 ];
 
-inquirer.prompt(questionsTeamManager).then((answers) => {
+inquirer.prompt(questions).then((answers) => {
     console.log(JSON.stringify(answers, null, '  '));
 });
 
